0a1228f981d88623e73e8f33e19ed96abd39a7fe
marq <classicvector63@gmail.com>
2024-10-30 22:21:10 +0530

    add dashboard page

--- src/app/dashboard/[workspaceId]/layout.tsx
+++ src/app/dashboard/[workspaceId]/layout.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { redirect } from "next/navigation";
+import { getNotifications, onAuthenticateUser } from "@/actions/user";
+import Sidebar from "@/components/global/sidebar";
+import GlobalHeader from "@/components/global/global-header";
+import {
+  getAllUserVideos,
+  getWorkspaceFolders,
+  getWorkSpaces,
+  verifyAccessToWorkspace,
+} from "@/actions/workspace";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+type Props = {
+  params: { workspaceId: string };
+  children: React.ReactNode;
+};
+const Layout = async ({ params: { workspaceId }, children }: Props) => {
+  const auth = await onAuthenticateUser();
+  if (!auth.user?.workspace) redirect("/auth/sign-in");
+  if (!auth.user.workspace.length) redirect("/auth/sign-in");
 
-const layout = () => {
+  const hasAccess = await verifyAccessToWorkspace(workspaceId);
+
+  if (hasAccess.status !== 200) {
+    redirect(`/dashboard/${auth.user?.workspace[0].id}`);
+  }
+
+  if (!hasAccess.data?.workspace) return null;
+  const query = new QueryClient();
+
+  await query.prefetchQuery({
+    queryKey: ["worksapce-folders"],
+    queryFn: () => getWorkspaceFolders(workspaceId),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-videos"],
+    queryFn: () => getAllUserVideos(workspaceId),
+  });
+  await query.prefetchQuery({
+    queryKey: ["user-workspaces"],
+    queryFn: () => getWorkSpaces(),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-notifications"],
+    queryFn: () => getNotifications(),
+  });
   return (
-    <div>layout</div>
-  )
-}
+    <HydrationBoundary state={dehydrate(query)}>
+      <div className="flex h-screen w-screen">
+        <Sidebar activeWorkspaceId={workspaceId} />
+        <div className="w-full pt-28 p-6 overflow-y-scroll overflow-x-hidden">
+          <GlobalHeader workspace={hasAccess.data.workspace} />
+          <div className="mt-4">{children}</div>
+        </div>
+      </div>
+    </HydrationBoundary>
+  );
+};
 
-export default layout
+export default Layout;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,17 @@
-import { onAuthenticateUser } from '@/actions/user'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-type Props = {}
+type Props = {};
 
 const DasboardPage = async (props: Props) => {
-  //Authentication of user 
-  const auth = await onAuthenticateUser()
+  //Authentication of user
+  const auth = await onAuthenticateUser();
   if (auth.status === 200 || auth.status === 201)
-    return redirect(`/dashboard/${auth.user?.workspace[0].id}`)
+    return redirect(`/dashboard/${auth.user?.workspace[0].id}`);
 
   if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
-    return redirect('/auth/sign-in')
+    return redirect("/auth/sign-in");
   }
-}
+};
 
-export default DasboardPage
+export default DasboardPage;
