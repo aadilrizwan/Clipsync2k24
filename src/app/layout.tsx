3bfd1a5c3680d57de21c59016570ea7aac564bd8
marq <classicvector63@gmail.com>
2024-10-30 22:41:51 +0530

    update app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 import { Manrope, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme";
+import ReactQueryProvider from "@/react-query";
+
 const manrope = DM_Sans({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "Clipsync",
   description:
@@ -18,18 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={`${manrope.className} bg-[#171717]`}>
-      <ThemeProvider
+      <html lang="en">
+        <body className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             disableTransitionOnChange
           >
-        {children}
-        </ThemeProvider>
+            <ReactQueryProvider>
+              {children}
+              {/* <Toaster /> */}
+            </ReactQueryProvider>
+          </ThemeProvider>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
-
   );
 }
