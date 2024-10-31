d665a9825c73262aa1d4325b94d061473a6f8330
Coder-2027 <codingninja2027@gmail.com>
2024-10-31 11:07:34 +0530

    Add create workspace feature

--- src/app/dashboard/[workspaceId]/layout.tsx
+++ src/app/dashboard/[workspaceId]/layout.tsx
@@ -18,7 +18,9 @@ type Props = {
   params: { workspaceId: string };
   children: React.ReactNode;
 };
-const Layout = async ({ params: { workspaceId }, children }: Props) => {
+const Layout = async ({ params, children }: Props) => {
+  const {workspaceId} = await params;
+  console.log(workspaceId);
   const auth = await onAuthenticateUser();
   if (!auth.user?.workspace) redirect("/auth/sign-in");
   if (!auth.user.workspace.length) redirect("/auth/sign-in");
--- src/app/dashboard/[workspaceId]/page.tsx
+++ src/app/dashboard/[workspaceId]/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getNotifications } from "@/actions/user";
+import {
+  getAllUserVideos,
+  getWorkspaceFolders,
+  getWorkSpaces,
+} from "@/actions/workspace";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import CreateWorkspace from '@/components/global/create-workspace'
 
-const page = () => {
+type Props = {
+  params: { workspaceId: string };
+};
+
+const Page = async ({ params }: Props) => {
+  const { workspaceId } = await params;
+  const query = new QueryClient();
+
+  await query.prefetchQuery({
+    queryKey: ["workspace-folders"],
+    queryFn: () => getWorkspaceFolders(workspaceId),
+  });
+
+  await query.prefetchQuery({
+    queryKey: ["user-videos"],
+    queryFn: () => getAllUserVideos(workspaceId),
+  });
   return (
-    <div>page</div>
-  )
-}
+    // <HydrationBoundary state={dehydrate(query)}>
+    <div>
+      <Tabs defaultValue="videos" className="mt-6">
+        <div className="flex w-full justify-between items-center">
+          <TabsList className="bg-transparent gap-2 pl-0">
+            <TabsTrigger
+              className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+              value="videos"
+            >
+              Videos
+            </TabsTrigger>
+            <TabsTrigger
+              value="archive"
+              className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+            >
+              Archive
+            </TabsTrigger>
+          </TabsList>
+          <div className="flex gap-x-3">
+            <CreateWorkspace />
+          </div>
+        </div>
+      </Tabs>
+    </div>
+    // {/* </HydrationBoundary> */}
+  );
+};
 
-export default page
+export default Page;
