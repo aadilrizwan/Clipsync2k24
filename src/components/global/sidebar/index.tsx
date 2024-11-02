38192ebf46cb56ab6236d26b163f346d37da06ff
abdulSamad2727 <abdulsamadansari936@gmail.com>
2024-11-02 12:55:49 +0530

    fixed error

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -63,7 +63,7 @@ const Sidebar = ({ activeWorkspaceId }: Props) => {
   }
 
   const SidebarSection = (
-    <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
+    <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center">
       <div className="bg-[#111111] p-4 flex gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0 ">
         <Image src="/logo.png" height={43} width={43} alt="logo" />
         <p className="text-2xl">ClipSync</p>
