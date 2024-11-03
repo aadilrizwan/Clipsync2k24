d6917d3490ee3e10eb016850dc818bb9d62debe1
Coder-2027 <codingninja2027@gmail.com>
2024-11-03 12:12:43 +0530

    Set up landing page

--- desktopclip/src/App.tsx
+++ desktopclip/src/App.tsx
@@ -3,13 +3,16 @@ import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import './App.css'
 import ControlLayout from './layouts/ControlLayout'
+import AuthButton from './components/Global/AuthButton'
+import Widget from './components/Global/Widget'
 const client = new QueryClient()
 function App() {
   return (
 <QueryClientProvider client={client}>
-  {/* <ControlLayout>
-    
-  </ControlLayout> */}
+  <ControlLayout>
+    <AuthButton />
+    <Widget/>
+  </ControlLayout>
   <Toaster/>
 </QueryClientProvider>
   )
--- desktopclip/src/layouts/ControlLayout.tsx
+++ desktopclip/src/layouts/ControlLayout.tsx
@@ -1,30 +1,48 @@
-import { cn } from '@/lib/utils'
-import { UserButton } from '@clerk/clerk-react'
-import React, { useState } from 'react'
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/clerk-react";
+import { X } from "lucide-react";
+import React, { useState } from "react";
+import { onCloseApp } from "@/lib/utils";
 
 type Props = {
-    children: React.ReactNode
-    classname?:string
-}
+  children: React.ReactNode;
+  classname?: string;
+};
 
-const ControlLayout = ({children,classname}: Props) => {
-    const [isVisible,setIsVisible] = useState<boolean>(false)
-    window.ipcRenderer.on("hide-plugin",(event,payload)=>{
-        console.log(event)
-        setIsVisible(payload.state)
-    })
+const ControlLayout = ({ children, classname }: Props) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  window.ipcRenderer.on("hide-plugin", (event, payload) => {
+    console.log(event);
+    setIsVisible(payload.state);
+  });
   return (
-    <div className={cn(classname,isVisible&& 'invisible',
-        'bg-[#171717] flex px-1 flex-col rounded-3xl overflow-hidden h-screen'
-    )}>
-      <div className='flex justify-between items-center p-5 draggable'>
-        <span className='non-draggable'>
-          <UserButton/>
+    <div
+      className={cn(
+        classname,
+        isVisible && "invisible",
+        "bg-[#171717] flex px-1 flex-col rounded-3xl overflow-hidden"
+      )}
+    >
+      <div className="flex justify-between items-center p-5 draggable">
+        <span className="non-draggable">
+          <UserButton />
         </span>
 
+        <X
+          size={20}
+          className="text-gray-400 non-draggable hover:text-white cursor-pointer"
+          onClick={onCloseApp}
+        />
+      </div>
+      <div className="flex-1 h-0 overflow-auto">{children}</div>
+      <div className="p-5 w-full flex">
+        <div className="flex items-center gap-x-2">
+          <img src="/logo.png" alt="ClipSync" className="w-10 h-10"/>
+          <p className="text-white text-2xl">ClipSync</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ControlLayout
+export default ControlLayout;
