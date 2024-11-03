be1f6fe2b02c1a7078b4f1086d2de174b39da0e3
Coder-2027 <codingninja2027@gmail.com>
2024-11-03 12:12:22 +0530

    Add components like loader, authbutton ...

---
+++ desktopclip/src/components/Global/Widget/index.tsx
@@ -0,0 +1,62 @@
+import { ClerkLoading, SignedIn, useUser } from "@clerk/clerk-react";
+import { Loader } from "../Loader";
+import { useEffect, useState } from "react";
+import { fetchUserProfile } from "@/lib/utils";
+import { useMediaSources } from "@/hooks/useMediaSources";
+
+const Widget = () => {
+  const [profile, setProfile] = useState<{
+    status: number;
+    user:
+      | ({
+          subscription: {
+            plan: "PRO" | "FREE";
+          } | null;
+          studio: {
+            id: string;
+            screen: string | null;
+            mic: string | null;
+            preset: "HD" | "SD";
+            camera: string | null;
+            userId: string | null;
+          } | null;
+        } & {
+          id: string;
+          firstname: string | null;
+          lastname: string | null;
+          email: string;
+          createdAt: Date;
+          clerkid: string;
+        })
+      | null;
+  } | null>(null);
+  const { user } = useUser();
+//   const {state, fetchMediaResources} = useMediaSources();
+
+  useEffect(() => {
+    if (user && user.id) {
+      fetchUserProfile(user.id).then((p) => setProfile(p));
+    }
+  }, [user]);
+
+  return (
+    <div className="p-5">
+      <ClerkLoading>
+        <div className="h-full flex justify-center items-center">
+          <Loader />
+        </div>
+      </ClerkLoading>
+      {/* <SignedIn>
+        {profile ? (
+          <MediaConfiguration />
+        ) : (
+          <div className="w-full h-full justify-center items-center">
+            <Loader color="#fff"></Loader>
+          </div>
+        )}
+      </SignedIn> */}
+    </div>
+  );
+};
+
+export default Widget;
