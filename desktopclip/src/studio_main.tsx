cd69611035876f1d4b641a1492b95925b7679fd1
abdulSamad2727 <abdulsamadansari936@gmail.com>
2024-11-07 23:16:39 +0530

    Add express server

--- desktopclip/src/studio_main.tsx
+++ desktopclip/src/studio_main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import {ClerkProvider} from '@clerk/clerk-react'
 import App from './studio_app'
 import './index.css'
+import { toast } from 'sonner'
  
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if(!PUBLISHABLE_KEY){
+  toast.error("Missing publishable key in studio_main");
   throw new Error("Missing publishable key")
 }
 
--- desktopclip/src/web_cam_main.tsx
+++ desktopclip/src/web_cam_main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import {ClerkProvider} from '@clerk/clerk-react'
 import App from './web_cam_app'
 import './index.css'
-
+import { toast } from 'sonner'
+ 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if(!PUBLISHABLE_KEY){
+  toast.error("Missing publishable key in main");
   throw new Error("Missing publishable key")
 }
 
