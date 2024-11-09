8c4ad34d8b2d0d3c807c43f083f72f9bf0698eae
marq <classicvector63@gmail.com>
2024-11-09 12:52:22 +0530

    implement pricing and dashboard pages

--- src/app/dashboard/[workspaceId]/home/page.tsx
+++ src/app/dashboard/[workspaceId]/home/page.tsx
@@ -1,9 +1,187 @@
-import React from 'react'
-
-const Home = () => {
+import Image from "next/image";
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+const TimelineDemo = () => {
+  const data = [
+    {
+      title: "Main",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Getting Started With clipsync ,Click login with suitable plugin
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/main.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/main2.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/main3.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/mainlogin.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "ClipVault",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            After Successfull login, user redirect to clipvaultpage
+          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Dashboard have lot of feature see in images like create workspace ,
+            upload and record video invite other client, manage account and take
+            a pro features
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/w1.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/w2.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/w3.png"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/w4.png"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Ai & Chatbot",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Premium version of clipsync
+          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            After creating workaspace and inside the given workspace create a
+            folder and capturing video and upload in folder and Ai generate
+            Title , Description, transcript summary
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/mainchat.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/a1.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/a2.png"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/home.png"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Desktop Clip",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Desktopclip is important component of clipsync
+          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            In desktop part of clipsync user can create a desktop and inside the
+            desktop capturing own video,voice and screen recording
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/d1.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/d2.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/d3.png"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/d4.png"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
   return (
-    <div>Home</div>
-  )
-}
-
-export default Home
+    <div className="w-full">
+      <Timeline data={data} />
+    </div>
+  );
+};
+export default TimelineDemo;
---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,378 @@
+"use client";
+import PaymentButton from "@/components/global/payment-button";
+import React, { useState } from "react";
+
+const Pricing = () => {
+  return (
+    <section className="py-10 bg-white sm:py-16 lg:py-24">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="max-w-xl mx-auto text-center">
+          <h2 className="text-4xl font-bold text-black lg:text-5xl sm:text-5xl">
+            Pricing &amp; Plans
+          </h2>
+          <p className="mt-4 text-lg leading-relaxed text-gray-600">
+            ClipSync offers a range of pricing plans designed to meet the needs
+            of individual users, small businesses, and large enterprises.
+          </p>
+        </div>
+        <div className="hidden mt-16 lg:block">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th className="py-8 pr-4"></th>
+
+                <th className="px-4 py-8 text-center">
+                  <span className="text-base font-medium text-blue-600">
+                    {" "}
+                    Free{" "}
+                  </span>
+                  <p className="mt-6 text-6xl font-bold  text-black">₹0</p>
+                  <p className="mt-2 text-base font-normal text-gray-500">
+                    Per month
+                  </p>
+                </th>
+
+                <th className="px-4 py-8 text-center">
+                  <span className="text-base font-medium text-blue-600">
+                    {" "}
+                    PRO{" "}
+                  </span>
+                  <p className="mt-6 text-6xl font-bold  text-black">₹4500</p>
+                  <p className="mt-2 text-base font-normal text-gray-500">
+                    Per month
+                  </p>
+                </th>
+
+                <th className="px-4 py-8 text-center bg-gray-900 rounded-t-xl">
+                  <span className="px-4 py-2 text-base font-medium text-white bg-blue-600 rounded-full">
+                    {" "}
+                    Popular{" "}
+                  </span>
+                  <p className="mt-6 text-6xl font-bold text-white  text-black">
+                    ₹5000
+                  </p>
+                  <p className="mt-2 text-base font-normal text-gray-200">
+                    Per month
+                  </p>
+                </th>
+
+                <th className="px-4 py-8 text-center">
+                  <span className="text-base font-medium text-blue-600">
+                    {" "}
+                    Enterprise{" "}
+                  </span>
+                  <p className="mt-6 text-6xl font-bold  text-black">₹7000</p>
+                  <p className="mt-2 text-base font-normal text-gray-500">
+                    Per month
+                  </p>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b text-black border-gray-200">
+                  Workspace
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  0
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  10
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  50
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Unlimited
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Server storage
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  10 GB
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  100 GB
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  1 TB
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Unlimited
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  File Sharing
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  7 Days
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  30 Days
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  90 Days
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Full with recovery
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Device sync
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  -
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  <svg
+                    className="w-5 h-5 mx-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  <svg
+                    className="w-5 h-5 mx-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200">
+                  <svg
+                    className="w-5 h-5 mx-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Support
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Chatbot support
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Priority Support
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  Priority support
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  24/7 premium support
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Video Recording
+                </td>
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  5 min
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  30 min
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  2 hr
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  10 hr
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Ai Feature
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  -
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Generate title and Description
+                </td>
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  Generate title ,Description with Transcript
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Fully Support
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-4 pr-4 font-medium border-b border-gray-200  text-black">
+                  Best For
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Individuals,Freelancers
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Small teams & individuals
+                </td>
+
+                <td className="px-4 py-4 text-center text-white bg-gray-900 border-b border-white/20">
+                  Growing Business & teams
+                </td>
+
+                <td className="px-4 py-4 text-center border-b border-gray-200  text-black">
+                  Large enterprises & organisations
+                </td>
+              </tr>
+
+              <tr>
+                <td className="py-6 pr-4"></td>
+
+                <td className="px-4 py-6 text-center">
+                  <a
+                    href="#"
+                    title=""
+                    className="inline-flex items-center font-semibold text-blue-600 hover:text-blue-700"
+                  >
+                    Get Started
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </td>
+
+                <td className="px-4 py-6 text-center">
+                  <a
+                    href="#"
+                    title=""
+                    className="inline-flex items-center font-semibold text-blue-600 hover:text-blue-700"
+                  >
+                    Get Started
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </td>
+
+                <td className="px-4 py-6 text-center text-white bg-yellow-500 rounded-b-xl">
+                  <a
+                    href="/payment"
+                    title=""
+                    className="inline-flex items-center font-semibold text-white"
+                  >
+                    Get Started
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </td>
+
+                <td className="px-4 py-6 text-center">
+                  <a
+                    href="#"
+                    title=""
+                    className="inline-flex items-center font-semibold text-blue-600 hover:text-blue-700"
+                  >
+                    Get Started
+                    <svg
+                      className="w-4 h-4 ml-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Pricing;
