1a3cc3c6e7706544e779f857abd0c89a7447ab71
abdulSamad2727 <abdulsamadansari936@gmail.com>
2024-11-09 18:48:27 +0530

    modified

--- src/app/dashboard/[workspaceId]/billing/page.tsx
+++ src/app/dashboard/[workspaceId]/billing/page.tsx
@@ -1,5 +1,7 @@
 
 import { getPaymentInfo } from '@/actions/user'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
+import { ArrowBigRight } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
@@ -20,8 +22,12 @@ const BillingPage = async (props: Props) => {
         <p className="text-[#9D9D9D]">{payment?.data?.subscription?.plan}</p>
       </div>
 
-      <a href='/pricing' className='text-pink-600 text-3xl border-2 rounded-md p-2 w-fit bg-pink-300 
-      shadow-[0_0_15px_10px_rgba(232,207,205,1)]'>Explore More About Prices</a>
+      <div className='flex items-center space-x-4'>
+  <a href='/pricing' className='text-white text-3xl border-2 rounded-md p-2 w-fit bg-black 
+  shadow-[0_0_15px_10px_rgba(232,207,205,1)]'>
+    Explore More About Prices ->
+  </a>
+</div>
     </div>
   )
 }
